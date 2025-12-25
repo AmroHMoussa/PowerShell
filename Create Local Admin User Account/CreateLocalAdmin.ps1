c4147b948562aff04a84dbde9575a5b7781980f0
AmroHMoussa <48489082+AmroHMoussa@users.noreply.github.com>
2025-12-25 16:06:48 +0200

    Add files via upload

---
+++ Create Local Admin User Account/CreateLocalAdmin.ps1
@@ -0,0 +1,17 @@
+﻿# Define variables
+$Username = "xxx" #Define the username
+$Password = "xxx" #Define the password
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+# Check if user already exists
+if (-Not (Get-LocalUser -Name $Username -ErrorAction SilentlyContinue)) {
+    # Create the local user with password never expiring
+    New-LocalUser -Name $Username -Password $SecurePassword -FullName "xxx" -Description "Local admin account created by script" -PasswordNeverExpires #Replace xxx with the Fullname
+    Write-Host "User '$Username' created successfully."
+} else {
+    Write-Host "User '$Username' already exists."
+}
+
+# Add user to Administrators group
+Add-LocalGroupMember -Group "Administrators" -Member $Username
+Write-Host "User '$Username' added to Administrators group."
