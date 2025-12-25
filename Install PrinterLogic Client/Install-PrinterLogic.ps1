7033f32f79c93cc9f203ea7374760a435a7c63a5
AmroHMoussa <48489082+AmroHMoussa@users.noreply.github.com>
2025-12-25 16:02:12 +0200

    Add files via upload

---
+++ Install PrinterLogic Client/Install-PrinterLogic.ps1
@@ -0,0 +1,19 @@
+﻿$MSIPath = "\\xxx\xxx\PrinterInstallerClient.msi" #Define your SMB or Hotlink
+$LogFile = "C:\temp\package.log"
+
+if (Test-Path "HKLM:\SOFTWARE\PrinterLogic\PrinterInstaller") {
+    exit 0
+}
+
+Start-Process -FilePath "msiexec.exe" -ArgumentList @(
+    "/i"
+    "`"$MSIPath`""
+    "/qn+"                                  
+    "ALLUSERS=1"
+    "HOMEURL=xxx" #Define your printercloud URL
+    "AUTHORIZATION_CODE=xxx" #Define your authorization code
+    "/l*v"
+    "`"$LogFile`""
+) -Wait -WindowStyle Hidden -PassThru | Out-Null
+
+exit 0
