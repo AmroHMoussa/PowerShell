550b7b6ea64e87020acccee9fd7899b14257313e
AmroHMoussa <48489082+AmroHMoussa@users.noreply.github.com>
2025-12-25 16:04:54 +0200

    Add files via upload

---
+++ Change Local Account Password/Reset Local User Account Password.ps1
@@ -0,0 +1,13 @@
+﻿# Define variables
+$Username = "xxx" #Define the local account user account
+$NewPassword = "xxx" #Define the password
+$SecurePassword = ConvertTo-SecureString $NewPassword -AsPlainText -Force
+
+# Check if the user exists
+if (Get-LocalUser -Name $Username -ErrorAction SilentlyContinue) {
+    # Set the new password
+    Set-LocalUser -Name $Username -Password $SecurePassword
+    Write-Host "Password for '$Username' has been successfully reset."
+} else {
+    Write-Host "User '$Username' does not exist."
+}
